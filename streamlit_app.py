304e4b3077b970b0356a83284804c1fd031bde3f
Phimphot Nualnim <151303038+LuciferSixth@users.noreply.github.com>
2024-08-29 14:10:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -12,14 +12,7 @@ st.header('Please upload an image of a knee X-ray')
 
 # Load Model
 device = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
-try:
-    model = torch.load('ghostnet_checkpoint_fold1.pt', map_location=device)
-except FileNotFoundError:
-    st.error("Model file not found. Please check the file path.")
-except RuntimeError as e:
-    st.error(f"Runtime error occurred: {e}")
-except Exception as e:
-    st.error(f"An unexpected error occurred: {e}")
+model = torch.load('ghostnet_checkpoint_fold1.pt', map_location=device)
 
 # Display image & Prediction
 uploaded_image = st.file_uploader('Choose an image', type=['jpg', 'jpeg', 'png'])
@@ -28,21 +21,18 @@ if uploaded_image is not None:
     image = Image.open(uploaded_image).convert('RGB')
     st.image(image, caption='Uploaded Image', use_column_width=True)
     
-    class_names = ['Normal', 'Mild', 'Severe']
+    class_name = ['Normal', 'Mild', 'Severe']
 
     if st.button('Predict'):
-        try:
-            # Prediction class
-            probli = pred_class(model, image, class_names)
-            st.write("## Prediction Result")
-            
-            # Get the index of the maximum value in probli[0]
-            max_index = np.argmax(probli[0])
+        # Prediction class
+        probli = pred_class(model, image, class_name)
+        
+        st.write("## Prediction Result")
+        # Get the index of the maximum value in probli[0]
+        max_index = np.argmax(probli[0])
 
-            # Iterate over the class_names and probli lists
-            for i in range(len(class_names)):
-                # Set the color to blue if it's the maximum value, otherwise use the default color
-                color = "blue" if i == max_index else None
-                st.write(f"## <span style='color:{color}'>{class_names[i]} : {probli[0][i]*100:.2f}%</span>", unsafe_allow_html=True)
-        except Exception as e:
-            st.error(f"An error occurred during prediction: {e}")
+        # Iterate over the class_name and probli lists
+        for i in range(len(class_name)):
+            # Set the color to blue if it's the maximum value, otherwise use the default color
+            color = "blue" if i == max_index else None
+            st.write(f"## <span style='color:{color}'>{class_name[i]} : {probli[0][i]*100:.2f}%</span>", unsafe_allow_html=True)
